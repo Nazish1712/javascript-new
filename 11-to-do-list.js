ff75f4dff025d1d1d0fc69b61007d32c6c3b646a
Nazish1712 <156432098+Nazish1712@users.noreply.github.com>
2024-02-20 18:37:14 +0530

    todo list changes

--- 11-to-do-list.js
+++ 11-to-do-list.js
@@ -1,13 +1,29 @@
-const todoList = ['make dinner'];  
+const todoList = [{
+    name:'make dinner', 
+    dueDate: '2024-02-20'},
+    { name:'wash dishes',
+    dueDate:'2024-02-20'}
+];  
 
 renderTodoList();
 
 function renderTodoList(){
-let todoListHTML = '';
+let todoListHTML = '';     //accumulator pattern so a variable to store html
 
 for (let i = 0; i < todoList.length; i++){        
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];    // to get each value(index) from the array todoList
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const {name , dueDate} = todoObject;
+    const html = 
+    `<div>${name}</div>
+     <div>${dueDate}</div> 
+     <button onclick="
+     todoList.splice(${i}, 1);
+     renderTodoList();
+     " class="delete-todo" >Delete</button>
+    `;
+    
     todoListHTML += html;
 }
 document .querySelector('.js-todo-list')
@@ -18,8 +34,16 @@ function addTodo(){
     const inputElement = document.querySelector
     ('.js-name-input'); 
     const name = inputElement.value;
+    
+    const dateInputElement = document.querySelector
+    ('.js-due-date-input');
 
-    todoList.push(name);
+    const dueDate = dateInputElement.value;
+
+    todoList.push(
+        {name, 
+        dueDate
+    });
     console.log(todoList);
 
     inputElement.value ='';
