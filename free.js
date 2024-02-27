ced286825834b1f4607c19919f31747b537a1344
Nazish1712 <156432098+Nazish1712@users.noreply.github.com>
2024-02-27 15:52:51 +0530

    exercises

--- free.js
+++ free.js
@@ -1,8 +1,17 @@
-const myArray = [10,20,30];
+/**const myArray = [10,20,30];
 
 myArray[2]=40;
 console.log(myArray);
 
 function getLastValue(array){
     
-}
+}**/
+ /**let myArray = ['hello','world','search','good'] 
+ for(let i = 0;i < myArray.length; i++){
+    const array = myArray[i];
+ if (array[i] = 'search'){
+    console.log(2);
+ }else {console.log(-1);}}**/
+
+ 
+
